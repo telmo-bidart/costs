d04846eafed114497836f9d3b80b170415d33060
telmo bidart <telmobidart@gmail.com>
2022-02-07 09:24:39 -0300

    react route to v6

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Company from './Components/Company';
+import Contact from './Components/Contact';
+import Home from './Components/Home';
+import NewProject from './Components/NewProject';
 
 function App() {
   return (
-    <div className="App">
-      <p>
-        Costs
-      </p>
-    </div>
+  <Router>
+   <div>
+     <Link to ='/'>Home</Link>
+     <Link to ='/contact'>Contact</Link>
+     <Link to ='/company'>Company</Link>
+     <Link to ='/newproject'>NewProject</Link>
+   </div>
+   <div></div>
+    <Routes>
+      <Route exact path='/'>
+        <Home />
+      </Route>
+      <Route path='/company'>
+        <Company />
+      </Route>
+      <Route path='/contact'>
+        <Contact />
+      </Route>
+      <Route path='/newproject'>
+        <NewProject />
+      </Route>
+    </Routes>
+  </Router>
   );
 }
 
